db301b15fe745c8719830fc4cb8ee3441648ff9a
Mathieu Lemay <acidrain1@gmail.com>
2025-07-19 09:13:21 -0400

    Add opentelemetry support for Flask

--- acidrain_logging/testing/flask/__init__.py
+++ acidrain_logging/testing/flask/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 from pydantic import BaseModel
 from structlog.stdlib import BoundLogger
 
-from acidrain_logging import LogConfig, configure_logger
+from acidrain_logging import LogConfig, configure_logger, configure_tracing
+from acidrain_logging.config import OtelConfig
 from acidrain_logging.flask.middlewares import add_log_middlewares
 
 app = Flask(__name__)
@@ -27,10 +28,15 @@ def get_value(key1: str, key2: str) -> str:  # noqa: ARG001 -> Unused args are o
     return Result().model_dump_json()
 
 
-def create_app(log_config: LogConfig | None = None) -> Flask:
+def create_app(
+    log_config: LogConfig | None = None, otel_config: OtelConfig | None = None
+) -> Flask:
     log_config = log_config or LogConfig()
+    otel_config = otel_config or OtelConfig()
 
     configure_logger(log_config)
+    configure_tracing(otel_config)
+
     add_log_middlewares(app)
 
     return app
--- tests/module/test_flask.py
+++ tests/module/test_flask.py
@@ -2,11 +2,13 @@ import json
 import socket
 from collections.abc import AsyncGenerator
 from datetime import UTC, datetime
+from typing import Any
 from unittest.mock import ANY
 from urllib.parse import urlparse
 
 import httpx
 import pytest
+import tenacity
 from httpx import AsyncClient, RequestError
 from pytest_docker.plugin import Services
 
@@ -36,6 +38,39 @@ def api_base_url(docker_ip: str, docker_services: Services) -> str:
 async def api_client(api_base_url: str) -> AsyncGenerator[AsyncClient, None]:
     async with httpx.AsyncClient(base_url=api_base_url) as client:
         yield client
+
+
+async def test_api_logging_uses_otel(
+    api_client: AsyncClient, docker_logs: DockerLogs
+) -> None:
+    timestamp = datetime.now(tz=UTC)
+
+    resp = await api_client.get("/")
+    assert resp.is_success
+
+    api_logs = docker_logs("flask", since=timestamp).split("\n")
+    assert len(api_logs) == 1
+    entry = json.loads(api_logs[0])
+
+    assert "otel.trace_id" in entry
+    assert "otel.span_id" in entry
+
+    def get_exported_spans() -> list[dict[str, Any]]:
+        logs = docker_logs("flask", since=timestamp)
+
+        return [
+            e
+            for e in map(json.loads, filter(None, logs.split("\n")))
+            if "kind" in e and e["kind"].startswith("SpanKind.")
+        ]
+
+    log_entries = tenacity.retry(
+        stop=tenacity.stop_after_delay(15),
+        retry=tenacity.retry_if_result(lambda x: x == []),
+    )(get_exported_spans)()
+
+    assert len(log_entries) > 0
+    assert log_entries[0]["resource"]["attributes"]["service.name"] == "test-flask"
 
 
 async def test_request_logging_includes_all_metadata(
